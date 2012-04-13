da642b28574ee048b1027c1c21695c0fede36ebc
Rahul jain <rahuljain.roy@gmail.com>
2012-04-13 18:08:26 +0530

    made footer.js and replace footer

---
+++ footer.js
@@ -0,0 +1,4 @@
+document.write("<span class='left'>&copy; 2010 <a href='index.html'>Rahul Jain</a></span>");
+document.write("<span class='center'><font size='2' face='verdana' aligh='right'>All the information is authentic to my knowledge and belief.</font></span>");
+document.write("<span class='right'><a href='#' align='right'>Back to Top</a></span>");
+
