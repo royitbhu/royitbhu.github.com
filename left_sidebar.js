13a36a49baff81a7664eee5a86d130737d5800ba
Rahul jain <rahuljain.roy@gmail.com>
2012-04-17 15:58:38 +0530

    contact and ubuntu pages updated

--- left_sidebar.js
+++ left_sidebar.js
@@ -2,7 +2,7 @@ document.write("<h1>Site Map</h1>");
 document.write("<ul>");
 	document.write("<li><a href='index.html'>Home</a></li>");
 		document.write("<li><a href='profile.html'>Profile</a></li>");
-		document.write("<li><a href='project.html'>Projects</a></li>");
+		document.write("<li><a href='project.html'>Work</a></li>");
 		document.write("<li><a href='fun.html'>Fun</a></li>");
 		document.write("<li><a href='./album/album.html' target='_blank'>Images</a></li>");
 		document.write("<li><a href='ubuntu.html'>Ubuntu stuff</a></li>");
