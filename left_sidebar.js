48c7db755f319bff70ae8a5d7d64acf1ae5d7838
Rahul jain <rahuljain.roy@gmail.com>
2012-04-20 19:11:27 +0530

    Photo Album updated

--- left_sidebar.js
+++ left_sidebar.js
@@ -4,7 +4,7 @@ document.write("<ul>");
 		document.write("<li><a href='profile.html'>Profile</a></li>");
 		document.write("<li><a href='project.html'>Work</a></li>");
 		document.write("<li><a href='fun.html'>Fun</a></li>");
-		document.write("<li><a href='./album/album.html' target='_blank'>Images</a></li>");
+		document.write("<li><a href='album.html' >Images</a></li>");
 		document.write("<li><a href='ubuntu.html'>Ubuntu stuff</a></li>");
 		document.write("<li><a href='miscellaneous.html'>Miscellaneous</a></li>");
 		document.write("<li><a href='contact.html'>Contact</a></li>");
