ba3c7bce6402b8e2e617fd7b551dd8e284afa887
Rahul jain <rahuljain.roy@gmail.com>
2012-04-24 06:37:00 +0530

    google authorship

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-document.write("<span class='left'>&copy; 2012 <a href='index.html'>Rahul Jain</a></span>");
+document.write("<span class='left'>&copy; 2012 <a href='https://plus.google.com/102151180938209382923?rel=author'>Rahul Jain</a></span>");
 document.write("<span class='center'><font size='2' face='verdana' align='right'>All the information is authentic to my knowledge and belief.</font></span>");
 document.write("<span class='right'><a href='#' align='right'>Back to Top</a></span>");
 
