9fc0133c0c561dfc3cbde27b7e72fc6291a83044
Rahul jain <rahuljain.roy@gmail.com>
2012-05-26 10:20:19 +0530

    Disqus added to projects and album

--- js/left_sidebar.js
+++ js/left_sidebar.js
@@ -5,7 +5,6 @@ document.write("<ul>");
 		document.write("<li><a href='project.html'>Work</a></li>");
 		document.write("<li><a href='fun.html'>Fun</a></li>");
 		document.write("<li><a href='album.html' >Photos</a></li>");
-		document.write("<li><a href='ubuntu.html'>Ubuntu stuff</a></li>");
 		document.write("<li><a href='links.html'>Links</a></li>");
 		document.write("<li><a href='http://royitbhu.blogspot.com' target='_blank'>Blog</a></li>");
 		document.write("<li><a href='contact.html'>Contact</a></li>");
