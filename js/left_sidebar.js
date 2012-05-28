e60bae96f50e3dbf657990bab67a3d4172c20e42
Rahul jain <rahuljain.roy@gmail.com>
2012-05-28 19:07:49 +0530

    blog url edited

--- js/left_sidebar.js
+++ js/left_sidebar.js
@@ -6,7 +6,7 @@ document.write("<ul>");
 		document.write("<li><a href='fun.html'>Fun</a></li>");
 		document.write("<li><a href='album.html' >Photos</a></li>");
 		document.write("<li><a href='links.html'>Links</a></li>");
-		document.write("<li><a href='http://royitbhu.blogspot.com' target='_blank'>Blog</a></li>");
+		document.write("<li><a href='http://blog.rahuljain.org' >Blog</a></li>");
 		document.write("<li><a href='contact.html'>Contact</a></li>");
 	document.write("</ul>");
 document.write("<a href='http://github.com/royitbhu' target='_blank'><img style='position: absolute; top: 0; left: 0; border: 0;' src='img/git.png' alt='Fork me on GitHub'></a>");
